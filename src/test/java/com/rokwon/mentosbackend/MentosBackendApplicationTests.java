b1ce77217aa95e8ea4caf42fc90052b7d400b0d4
RokwonK <wonrok97@naver.com>
2023-06-08 01:26:41 +0900

    🎉 init

---
+++ src/test/java/com/rokwon/mentosbackend/MentosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rokwon.mentosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
