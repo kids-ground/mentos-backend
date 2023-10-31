e93a87d0e92d3ecf83350f726441bdcb09253e34
RokwonK <wonrok97@naver.com>
2023-10-31 23:32:22 +0900

    ✨ domain module 추가

---
+++ mentos-domain/src/main/java/com/rokwonk/MentosDomainApplication.java
@@ -0,0 +1,12 @@
+package com.rokwonk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentosDomainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MentosDomainApplication.class, args);
+    }
+
+}
