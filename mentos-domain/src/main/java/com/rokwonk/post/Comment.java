6808b09281614547e6246ca17c8728a60dc27cd1
RokwonK <wonrok97@naver.com>
2023-11-01 23:24:38 +0900

    ✨ post 관련 모델 추가

---
+++ mentos-domain/src/main/java/com/rokwonk/post/Comment.java
@@ -0,0 +1,39 @@
+package com.rokwonk.post;
+
+import com.rokwonk.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Table(name = "comment")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private Member writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tagged_member_id")
+    private Member taggedMember;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+}
---
+++ mentos-domain/src/main/java/com/rokwonk/post/Post.java
@@ -0,0 +1,45 @@
+package com.rokwonk.post;
+
+import com.rokwonk.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Table(name = "post")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(length = 30)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ColumnDefault("0")
+    private Integer hit;
+
+    private Integer commentCount;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "post_id")
+    private List<Comment> commentList;
+}
