d7b166c003a94bbb20c9814107fcc9053ac6562b
RokwonK <wonrok97@naver.com>
2023-11-03 21:44:03 +0900

    ✨ AuthService 내 회원가입 메서드 추가

--- mentos-api/src/main/java/com/rokwonk/service/AuthService.java
+++ mentos-api/src/main/java/com/rokwonk/service/AuthService.java
@@ -1,20 +1,69 @@
 package com.rokwonk.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rokwonk.client.AppleClient;
+import com.rokwonk.client.KakaoClient;
+import com.rokwonk.dto.internal.AppleJwtBodyInfo;
 import com.rokwonk.dto.request.LoginRequest;
 import com.rokwonk.dto.request.LoginType;
+import com.rokwonk.dto.response.AppleAuthKeysResponse;
+import com.rokwonk.dto.response.KakaoUserInfoResponse;
 import com.rokwonk.dto.response.TokenResponse;
+import com.rokwonk.exception.inavlid.InvalidApplePublicKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.PublicKey;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class AuthService {
 
-    @Transactional
+    private final ObjectMapper objectMapper;
+    private final OpenIdVerifyService openIdVerifyService;
+    private final JwtTokenService jwtTokenService;
+    private final MemberService memberService;
+
+    private final AppleClient appleClient;
+    private final KakaoClient kakaoClient;
+
     public TokenResponse loginOrSignUp(LoginRequest request) {
-        return new TokenResponse("");
+        LoginType oauthType = request.loginType();
+        String uniqueId = switch (oauthType) {
+            case KAKAO -> getKakaoUniqueUserId(request.token());
+            case APPLE -> getAppleUniqueUserId(request.token());
+        };
+
+        Long memberId = memberService.getMemberByOAuth(oauthType, uniqueId)
+                .orElseGet(() -> memberService.createMember(oauthType, uniqueId));
+        String accessToken = jwtTokenService.createToken(memberId);
+
+        return new TokenResponse(accessToken, null);
+    }
+
+    private String getAppleUniqueUserId(String identityToken) {
+        AppleAuthKeysResponse authKeys = appleClient.getAuthKeys();
+        String jwtHeaderString = jwtTokenService.extractJwtHeaderString(identityToken);
+        PublicKey publicKey = openIdVerifyService.getApplePublicKey(jwtHeaderString, authKeys);
+        Boolean isVerified  = jwtTokenService.verifyToken(identityToken, publicKey);
+
+        if (!isVerified) throw new InvalidApplePublicKeyException();
+
+        try {
+            String jwtBodyString = jwtTokenService.extractJwtBodyString(identityToken);
+            AppleJwtBodyInfo jwtBody = objectMapper.readValue(jwtBodyString, AppleJwtBodyInfo.class);
+            return jwtBody.getSub();
+        } catch(JsonProcessingException e) {
+            throw new InvalidApplePublicKeyException();
+        }
+    }
+
+    private String getKakaoUniqueUserId(String accessToken) {
+        KakaoUserInfoResponse userInfo = kakaoClient.getUserProfile("Bearer " + accessToken);
+        return userInfo.getId().toString();
     }
 }
